00fbd6be31ad74e8ae44e0962fc1b5d1d18ae6bd
Yassin Elsawy <yassinsawy@outlook.com>
2025-05-13 06:27:06 +0300

    Update Debt.java

--- untitled/src/Budgeting_Functionalities/Debt.java
+++ untitled/src/Budgeting_Functionalities/Debt.java
@@ -1,7 +1,38 @@
 package Budgeting_Functionalities;
 
+import java.time.LocalDate;
+
 public class Debt {
-    public float getAmount() {
-        
+    private int debtID;
+    private String category;
+    private float totalAmount;
+    private float remainingAmount;
+    private LocalDate dueDate;
+
+    // Default constructor required for Jackson deserialization
+    public Debt() {
     }
+    public Debt(int debtID, String category, float totalAmount, float remainingAmount, LocalDate dueDate) {
+        this.debtID = debtID;
+        this.category = category;
+        this.totalAmount = totalAmount;
+        this.remainingAmount = remainingAmount;
+        this.dueDate = dueDate;
+    }
+    public int getDebtID() {
+        return debtID;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public float getAmount() {
+        return totalAmount;
+    }
+    public float getRemainingAmount() {
+        return remainingAmount;
+    }
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
 }
