ef14779cbe715e928a4614cae5b5353d54a134d7
Abdelrahman <abdelrahmanyr05@gmail.com>
2025-05-13 03:38:21 +0300

    Mashy

--- untitled/src/User_Management/User.java
+++ untitled/src/User_Management/User.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class User {
