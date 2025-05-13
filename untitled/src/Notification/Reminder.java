8d60fb254de5eb02b18f329b080f3aaf6513b990
Abdelrahman <abdelrahmanyr05@gmail.com>
2025-05-13 20:32:10 +0300

    javaDoc comments for `Reminder` and `ReminderManager`

--- untitled/src/Notification/Reminder.java
+++ untitled/src/Notification/Reminder.java
@@ -1,35 +1,65 @@
 package Notification;
 
-import java.sql.Time;
-import java.util.Date;
+    import java.sql.Time;
+    import java.util.Date;
 
-public class Reminder {
-    private int reminderId;
-    private String reminderTitle;
-    private Date reminderDate;
-    private Time reminderTime;
+    /**
+     * Represents a reminder with a title, date, and time.
+     */
+    public class Reminder {
+        private int reminderId;
+        private String reminderTitle;
+        private Date reminderDate;
+        private Time reminderTime;
 
-    public Reminder(int reminderId, String reminderTitle, Date reminderDate, Time reminderTime) {
-        this.reminderId = reminderId;
-        this.reminderTitle = reminderTitle;
-        this.reminderDate = reminderDate;
-        this.reminderTime = reminderTime;
+        /**
+         * Constructs a new Reminder.
+         *
+         * @param reminderId    The unique ID of the reminder.
+         * @param reminderTitle The title of the reminder.
+         * @param reminderDate  The date of the reminder.
+         * @param reminderTime  The time of the reminder.
+         */
+        public Reminder(int reminderId, String reminderTitle, Date reminderDate, Time reminderTime) {
+            this.reminderId = reminderId;
+            this.reminderTitle = reminderTitle;
+            this.reminderDate = reminderDate;
+            this.reminderTime = reminderTime;
+        }
+
+        /**
+         * Gets the unique ID of the reminder.
+         *
+         * @return The reminder ID.
+         */
+        public int getReminderId() {
+            return reminderId;
+        }
+
+        /**
+         * Gets the title of the reminder.
+         *
+         * @return The reminder title.
+         */
+        public String getReminderTitle() {
+            return reminderTitle;
+        }
+
+        /**
+         * Gets the date of the reminder.
+         *
+         * @return The reminder date.
+         */
+        public Date getReminderDate() {
+            return reminderDate;
+        }
+
+        /**
+         * Gets the time of the reminder.
+         *
+         * @return The reminder time.
+         */
+        public Time getReminderTime() {
+            return reminderTime;
+        }
     }
-
-    // Getters
-    public int getReminderId() {
-        return reminderId;
-    }
-
-    public String getReminderTitle() {
-        return reminderTitle;
-    }
-
-    public Date getReminderDate() {
-        return reminderDate;
-    }
-
-    public Time getReminderTime() {
-        return reminderTime;
-    }
-}
--- untitled/src/Notification/ReminderManager.java
+++ untitled/src/Notification/ReminderManager.java
@@ -5,8 +5,21 @@ import java.sql.Time;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * Manages reminders for a user, including adding new reminders.
+ */
 public class ReminderManager {
 
+    /**
+     * Adds a new reminder to the specified user.
+     *
+     * @param user          The user to whom the reminder will be added.
+     * @param reminderId    The unique ID of the reminder.
+     * @param reminderTitle The title of the reminder.
+     * @param reminderDate  The date of the reminder.
+     * @param reminderTime  The time of the reminder.
+     * @throws IllegalArgumentException If the user is null.
+     */
     public void addReminder(User user, int reminderId, String reminderTitle, Date reminderDate, Time reminderTime) {
         if (user == null) {
             throw new IllegalArgumentException("User cannot be null.");
